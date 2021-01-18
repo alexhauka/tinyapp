5d727efc97211b303f8a207bcc8a4fbc8390c776
alexhauka <alexhauka@gmail.com>
2021-01-18 21:57:56 +0000

    added urls_show.ejs and built href for long & short url. Updated express server to render these

--- express_server.js
+++ express_server.js
@@ -19,13 +19,14 @@ app.get('/urls', (req, res) => { // => page displaying urls
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render('urls_show', templateVars);
+})
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
-
-// app.get('/hello', (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
