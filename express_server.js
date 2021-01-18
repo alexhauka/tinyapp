a7a0f2021b6ebdae09f2345687b6397d9644d9c6
alexhauka <alexhauka@gmail.com>
2021-01-18 23:13:14 +0000

    updated new url page to accept POST requests. Installed body-parser library. Updated server file accordingly.

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -17,6 +19,11 @@ app.get('/', (req, res) => { // => registers handler on root path ('/')
 app.get('/urls', (req, res) => { // => page displaying urls
   const templateVars = {urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get('/urls/new', (req, res) => {
