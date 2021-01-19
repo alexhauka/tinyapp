af85613e741d3c31b035afbe9cb5c3130095e5df
alexhauka <alexhauka@gmail.com>
2021-01-19 01:46:06 +0000

    added delete buttons

--- express_server.js
+++ express_server.js
@@ -42,9 +42,15 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
+})
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
 })
 
 app.get('/urls.json', (req, res) => {
