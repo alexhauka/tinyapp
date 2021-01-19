945f67a0d37f2e5c9515cb791338e7ade71d9b71
alexhauka <alexhauka@gmail.com>
2021-01-19 00:15:42 +0000

    added redirecting to the long URL from short URL

--- express_server.js
+++ express_server.js
@@ -37,6 +37,11 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
