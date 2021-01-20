299d7d4b4f57580e53a95dde616dbd4177b13c8c
alexhauka <alexhauka@gmail.com>
2021-01-20 00:35:19 +0000

    linted server file.

--- express_server.js
+++ express_server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser())
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 
-function generateRandomString() {
+const generateRandomString = function() {
   return Math.floor((1 + Math.random()) * 0x100000).toString(16);
 };
 
@@ -24,13 +24,13 @@ app.get('/', (req, res) => { // => registers handler on root path ('/')
 
 app.post('/login', (req, res) => {
   res.cookie('username', req.body.username);
-  res.redirect('/urls')
-})
+  res.redirect('/urls');
+});
 
 app.post('/logout', (req, res) => {
   res.clearCookie('username', req.cookies);
-  res.redirect('/urls')
-})
+  res.redirect('/urls');
+});
 
 app.get('/urls', (req, res) => { // => page displaying urls
   const templateVars = {
@@ -43,7 +43,7 @@ app.get('/urls', (req, res) => { // => page displaying urls
 app.post("/urls", (req, res) => {
   // console.log(req.body);  // Log the POST request body to the console
   let short = generateRandomString();
-  urlDatabase[short] = req.body.longURL
+  urlDatabase[short] = req.body.longURL;
   res.redirect(`/urls/${short}`);
   // console.log(urlDatabase)
 });
@@ -68,9 +68,9 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.post('/urls/:shortURL', (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.longURL
-  res.redirect('/urls')
-})
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect('/urls');
+});
 
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
@@ -79,12 +79,12 @@ app.get('/urls/:shortURL', (req, res) => {
     username: req.cookies['username']
   };
   res.render('urls_show', templateVars);
-})
+});
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
-})
+});
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
